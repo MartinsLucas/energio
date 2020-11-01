2d61fb8596e91663c49716061439b5259be055b8
Lucas Martins <lucas465@hotmail.com>
2020-10-31 21:53:20 -0300

    [BDGD Conversion] Adding prompt messages to conversion script

--- scripts/convert_bdgd.sh
+++ scripts/convert_bdgd.sh
@@ -3,25 +3,31 @@
 BDGD_SRC_BASE="./src/bdgd/latest/"
 BDGD_CONV_BASE="./src/bdgd/converted/"
 
+echo "Reading .gdb bases..."
 for entry in "$BDGD_SRC_BASE"*.gdb; do
-    bdgd_name="${entry%.gdb}"
-    bdgd_name="${bdgd_name#*latest/}"
+    bdgd_name="${entry#*latest/}"
+
+    echo "Found $bdgd_name..."
+
+    bdgd_name="${bdgd_name%.gdb}"
     bdgd_dir="$BDGD_CONV_BASE$bdgd_name"
 
+    echo "Checking if destination directory already exists."
     if [ -d $bdgd_dir ]; then
-        echo "exists"
+        echo "Directory exists."
     else
-        echo "doesn't exists"
-        echo "creating"
+        echo "Directory doesn't exists."
+        echo "Creating it."
         mkdir $bdgd_dir
     fi
 
-    echo $entry
+    echo "Retrieving layers from $bdgd_name"
     ogrinfo -q $entry | while read line; do
         layer=${line#*:\ }
         layer=${layer%\ (*}
 
-        echo "creating $bdgd_dir/$layer"
+        echo "Converting layer: $layer"
         ogr2ogr "$bdgd_dir/$layer" -f GeoJSON -t_srs EPSG:4674 $entry $layer
+        echo "$layer converted!"
     done
 done
