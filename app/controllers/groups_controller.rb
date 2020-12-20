798c2440266c38fa876c5373bd7a8b42fddeb484
Lucas Martins <lucas465@gmail.com>
2020-12-20 01:26:26 -0300

    Formats files

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,54 +5,14 @@ class GroupsController < ApplicationController
 
   # GET /groups
   def index
-    # print '#' * 30
-    # puts 'Encontrando pontos...'
-    # groups = Group.all
-    # points = NotablePoint.where(conj: groups.first.cod_id).first(10)
-
-    # count = points.count
-    # print '#' * 30
-    # print count
-    # puts ' pontos encontrados.'
-
-    # print '#' * 30
-    # puts 'Iniciando montagem...'
-
-    # collection = []
-    # factory = RGeo::GeoJSON::EntityFactory.instance
-
-    # print count
-    # puts ' pontos restantes...'
-
-    # groups.each do |group|
-    #   properties = {}
-    #   properties[:name] = group.nom if group.nom.present?
-    #   properties[:description] = group.descr if group.descr.present?
-    #   collection << factory.feature(group.geometry, group.cod_id, properties)
-    # end
-
-    # print '#' * 30
-    # puts 'Iniciando conversao...'
-    # feature_collection = RGeo::GeoJSON::FeatureCollection.new(collection)
-    # hash = RGeo::GeoJSON.encode feature_collection
-
-    # print '#' * 30
-    # puts 'Conversao concluida. Renderizando json...'
-    # render json: hash.to_json
-
     groups = Group.all
 
     feature = Group.create_rgeo_feature_colection(groups)
-    render json: Group.genetrates_geojson(feature)
+    render json: Group.genetrates_geojson(feature).to_json
   end
 
   def show
     feature = @group&.create_rgeo_feature
-
-    puts '#' * 30
-    puts Group.genetrates_geojson(feature)
-    puts '#' * 30
-
     render json: Group.genetrates_geojson(feature)
   end
 
--- app/models/substation.rb
+++ app/models/substation.rb
@@ -1,2 +1,3 @@
 class Substation < ApplicationRecord
+    include RgeoFunctions
 end
